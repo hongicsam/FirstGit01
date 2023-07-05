d7fb1bc182e5a40c0d4643d249f06fbff2715894
hongicsam1514 <hongicsam1514@gmail.com>
2023-07-05 16:24:44 +0900

    오타수정

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,7 +3,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 
-		System.out.println("1.프로젝스생성");
+		System.out.println("1.프로젝트생성");
 		System.out.println("2.수정");
 		System.out.println("3.push성공기념");
 		
